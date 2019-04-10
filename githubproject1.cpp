4e482009481c1c343b6ff515b184b9784aae3580
ayushi1233 <48521923+ayushi1233@users.noreply.github.com>
2019-04-10 17:06:30 +0530

    Add files via upload

---
+++ githubproject1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+      int at[10], bt[10];//at stands for arrival time and bt stands for burst time
+      int i, smallest, n;
+      double wt = 0;//wt=waiting time
+	  double ta = 0;//ta=turnaround time
+	  double end;
+      float avgwt, avgta;//avgwt=average waiting time  avgta=average turnaround time
+      cout<<"Enter the Total Number of Processes"<<endl;
+      cin>>n; //max no of processes
+      cout<<"Enter Details of  Processes"<<endl;
+      int temp[10];//temporary variable for storing burst time
+      for(i = 0; i < n; i++)
+      {
+            cout<<"Enter Arrival Time for "<<" process  "<<i<<endl;
+            cin>>at[i];//taking user input for arrival time
+            cout<<"Enter Burst Time for "<<" process  "<<i<<endl;
+            cin>>bt[i];//taking user input for burst time
+            temp[i] = bt[i];
+      }
+      bt[9] = 9999; 
+	  int t; //t represents time
+      int count=0;
+      for(t = 0; count != n; t++)
+      {
+            smallest = n-1;
+            for(i = 0; i < n; i++)
+            {
+                  if(at[i] <= t && bt[i] < bt[smallest] && bt[i] > 0)
+                  {
+                        smallest = i;
+                  }
+            }
+            bt[smallest]--;
+            if(bt[smallest] == 0)
+            {
+                  count++;
+                  end = t + 1;
+                  wt = wt + end - at[smallest] - temp[smallest];
+                  ta = ta + end - at[smallest];
+            }
+      }
+      avgwt = wt / n; 
+      avgta = ta / n;
+      cout<<"Average Waiting Time:"<< avgwt<<endl;
+      cout<<"Average Turnaround Time:"<< avgta<<endl;;
+      return 0;
+}
